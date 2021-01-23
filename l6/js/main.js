32c57a9a69b894c3f1522584ef25a63449b4fd68
sunder <alexander@darklight.by>
2021-01-23 20:23:42 +0300

    Add home work files

--- l6/js/main.js
+++ l6/js/main.js
@@ -2,9 +2,29 @@ let user = {
   name: 'Elon',
   secondName: 'Musk',
   city: 'Palo-Alto',
-  birthDate: '19.02.1976'
+  birthDate: '19.02.1976',
+  profileImage: '', // прочитать про свойство src у картинок. В него можно записывать значение строки адреса картинки
+  // Укажите любые ваши значения, вместо пустых строк. Примените стили к блоку с id setting-page
+  userSettings: {
+    backgroundColor: '',
+    color: '',
+    fontSize: ''
+  },
+  loadUserData: function () {
+    // Код метода установки значений с информацией о пользователе
+    let userName = document.getElementById('user-name');
+
+    userName.innerHTML = `${user.name} ${user.secondName}`;
+
+    // Ваш код здесь
+  },
+  loadUserSettings: function () {
+    // Код метода установки настроек страницы
+  },
+  load() {
+    this.loadUserData();
+    this.loadUserSettings();
+  }
 };
 
-let userName = document.getElementById('user-name');
 
-userName.innerHTML = `${user.name} ${user.secondName}`;
