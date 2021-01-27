70d4cad355c57562c5af4b23805fde42c6566188
sunder <alexander@darklight.by>
2021-01-27 19:22:29 +0300

    add homework result

--- l2/hw2/cinema.js
+++ l2/hw2/cinema.js
@@ -1,9 +1,22 @@
 let films = ['Один дома', 'Король лев'];
 let filmsString = `Добро пожаловать в "Дом Кино". Сегодня в афише ${films.join(', ')}`;
 
+let cinema = {
+  films: ['Один дома', 'Король лев'],
+  filmsString: `Добро пожаловать в "Дом Кино". Сегодня в афише ${films.join(', ')}`,
+  isExistingFilm: function(userFilm) {
+    // Ищем индекс в массиве films. Возввращаем true или false, сравнивая с -1 (минус один получаем тогда, когда искомого элемента нет в массиве)
+    return films.findIndex(function(film) {
+      return film === userFilm;
+    }) !== -1;
+  }
+
+};
+
 // Показываем афишу
 alert(filmsString);
 
+// Здесь задаю вопрос у пользователя
 let userFilmChoice = prompt('Выберите фильм, на который хотите пойти');
 
 function isExistingFilm(userFilm) {
@@ -28,7 +41,11 @@ function getFilmIdx(userFilm) {
 if (isExistingFilm(userFilmChoice)) {
   // ... Здесь ваш код дальше
 
-  let userRowChoice = prompt('...');
+  let userRowChoice = prompt('Выберите ряд, на который хотите пойти');
+  let userPlaceChoice = prompt('Выберите место, на который хотите сесть');
+  let isWantPopcorn = confirm('Желаете ли попкорн?');
+
+  alert(`Вы пойдете на фильм: ${userFilmChoice}. Ваш ряд: ${userRowChoice}, userPlaceChoice, isWantPopcorn`);
 } else {
   alert('Извините, сегодня в кино такого фильма нет.')
 }
--- l6/js/main.js
+++ l6/js/main.js
@@ -3,28 +3,35 @@ let user = {
   secondName: 'Musk',
   city: 'Palo-Alto',
   birthDate: '19.02.1976',
-  profileImage: '', // прочитать про свойство src у картинок. В него можно записывать значение строки адреса картинки
+  profileImage: 'https://static.theceomagazine.net/wp-content/uploads/2018/10/15093202/elon-musk.jpg', // прочитать про свойство src у картинок. В него можно записывать значение строки адреса картинки
   // Укажите любые ваши значения, вместо пустых строк. Примените стили к блоку с id setting-page
   userSettings: {
-    backgroundColor: '',
-    color: '',
-    fontSize: ''
+    backgroundColor: '#b2b2b2',
+    color: '#010115',
+    fontSize: '100px'
   },
   loadUserData: function () {
     // Код метода установки значений с информацией о пользователе
     let userName = document.getElementById('user-name');
 
-    userName.innerHTML = `${user.name} ${user.secondName}`;
+    userName.innerHTML = `${this.name} ${this.secondName}`;
 
-    // Ваш код здесь
+    let userImage = document.getElementById('user-image');
+
+    userImage.src = this.profileImage;
   },
   loadUserSettings: function () {
-    // Код метода установки настроек страницы
+    let userPageElement = document.getElementById('setting-page');
+    userPageElement.style.background = this.userSettings.backgroundColor;
+    userPageElement.style.color = this.userSettings.color;
+    userPageElement.style.fontSize = this.userSettings.fontSize;
   },
   load() {
     this.loadUserData();
     this.loadUserSettings();
   }
 };
+
+user.load();
 
 
